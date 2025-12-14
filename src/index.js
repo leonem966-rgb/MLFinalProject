0426725c0177c0f7d1b1687c4cd5f222fcd96ffc
leonem966-rgb <leonem966@gmail.com>
2025-12-13 20:01:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function displayCityTemperature(response) {
    let descriptionElement = document.querySelector("#description");
    let humidityElement = document.querySelector("#humidity");
    let windSpeedElement = document.querySelector("#wind-speed");
-  let iconElement = document.querySelector("#icon");
+   let iconElement = document.querySelector("#icon");
 
   
 cityElement.innerHTML = response.data.city;  
@@ -18,6 +18,7 @@ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;  
 iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temp-icon"/>`;
 
+getForecast(response.data.city);  
 }   
 
 function formatDate(date) {
@@ -53,15 +54,55 @@ function searchCurrentCity(cityElement) {
   axios.get(apiUrl).then(displayCityTemperature);
   }
 
-
 function search(event) {
 event.preventDefault();
 let searchInputElement = document.querySelector("#search-input");
 searchCurrentCity(searchInputElement.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "abtb6a92443361bbdo4b8fd76decc014";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+
+  let forecastHTML = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+  forecastHTML =
+  forecastHTML + 
+  `
+  <div class="range-forecast-day">
+      <div class="range-forecast-date">${formatDay(day.time)}</div>
+
+      <img src="${day.condition.icon_url}" class="range-forecast-icon" />
+      <div class="range-forecast-temperatures">
+        <div class="range-forecast-temperature">
+          <strong>${Math.round(day.temperature.maximum)}°</strong>
+        </div>
+        <div class="range-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
+     </div>
+  </div>
+`;
+  });
+
+let forecastElement = document.querySelector("#forecast");    
+forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", search);
 
 searchCurrentCity("Kingston");
+getForecast("Kingston");
+displayForecast();
 
